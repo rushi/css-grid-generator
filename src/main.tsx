4110be07e9721a57dbab56fd8ebcd93ef5b9e1cb
Rushi Vishavadia <rushi@xola.com>
2025-08-02 15:36:57 +0530

    Test @vercel/analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
+import { Theme } from "@radix-ui/themes";
+import "@radix-ui/themes/styles.css";
+import { Analytics } from "@vercel/analytics/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
-import "@radix-ui/themes/styles.css";
-import { Theme } from "@radix-ui/themes";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <Theme>
             <App />
+            <Analytics />
         </Theme>
     </React.StrictMode>,
 );
