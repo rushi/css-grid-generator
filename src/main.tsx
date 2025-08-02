675a3edecd80907a34be3b12ce6338099977d292
Rushi Vishavadia <rushi@xola.com>
2025-08-02 15:36:57 +0530

    Test @vercel/analytics
    Add @vercel/speed-insights

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
+import { Theme } from "@radix-ui/themes";
+import "@radix-ui/themes/styles.css";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
-import "@radix-ui/themes/styles.css";
-import { Theme } from "@radix-ui/themes";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <Theme>
             <App />
+            <Analytics />
+            <SpeedInsights />
         </Theme>
     </React.StrictMode>,
 );
