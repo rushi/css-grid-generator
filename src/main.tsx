e5161c32692226680e20134714d5674e6f85b2c1
Rushi Vishavadia <rushi@xola.com>
2025-08-03 08:43:58 +0530

    Posthog

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,29 @@
+/// <reference types="vite/types/importMeta.d.ts" />
 import { Theme } from "@radix-ui/themes";
 import "@radix-ui/themes/styles.css";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { PostHogProvider } from "posthog-js/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <Theme>
-            <App />
-            <Analytics />
-            <SpeedInsights />
-        </Theme>
+        <PostHogProvider
+            apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+            options={{
+                api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+                defaults: "2025-05-24",
+                capture_exceptions: true,
+                debug: import.meta.env.MODE === "development",
+            }}
+        >
+            <Theme>
+                <App />
+                <Analytics />
+                <SpeedInsights />
+            </Theme>
+        </PostHogProvider>
     </React.StrictMode>,
 );
