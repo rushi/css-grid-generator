fffc378d59f8f0f743d398e1e8e17888aaf31e40
Rushi Vishavadia <rushi@xola.com>
2025-08-03 11:23:24 +0530

    Update header

--- src/App.tsx
+++ src/App.tsx
@@ -160,7 +160,10 @@ const App = () => {
                 <header className="border-light-gray border-b">
                     <div className="mx-auto max-w-7xl px-4 py-4">
                         <h2>CSS Grid Generator</h2>
-                        <p className="text-extra-dark-gray mt-2">Create custom CSS grid layouts with drag & drop</p>
+                        <p className="text-extra-dark-gray mt-2">
+                            Create custom CSS grid layouts with drag, drop & resize. Generate code for Tailwind CSS or
+                            standard CSS + HTML.
+                        </p>
                     </div>
                 </header>
 
